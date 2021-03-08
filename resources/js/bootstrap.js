5ca4d33859bf16e63cbd89bbde982a1e7c039ec2
Edwar Roa <eroa1@udi.edu.co>
2021-03-08 11:45:09 -0500

    Actualización de node
    Configuré el node, para las dependencias, Y configuré mi equipo con la base de datos

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,14 +1,25 @@
 window._ = require('lodash');
 
 /**
+ * We'll load jQuery and the Bootstrap jQuery plugin which provides support
+ * for JavaScript based Bootstrap features such as modals and tabs. This
+ * code may be modified to fit the specific needs of your application.
+ */
+
+try {
+    window.Popper = require('popper.js').default;
+    window.$ = window.jQuery = require('jquery');
+
+    require('bootstrap');
+} catch (e) {}
+
+/**
  * We'll load the axios HTTP library which allows us to easily issue requests
  * to our Laravel back-end. This library automatically handles sending the
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
 window.axios = require('axios');
-
-window.vue= require('vue');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
@@ -18,8 +29,6 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
  * allows your team to easily build robust real-time web applications.
  */
 
- Vue.component('desactivar-usuario', require('./components/DesactivarUsuario.vue').default);
- Vue.component('estado-actividad', require('./components/EstadoActividad.vue').default);
 // import Echo from 'laravel-echo';
 
 // window.Pusher = require('pusher-js');
@@ -30,17 +39,3 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 //     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
 //     forceTLS: true
 // });
-
-
-const app=new Vue({
-    el: '#app',
-});
-
-
-
-
-
-
-
-
-
